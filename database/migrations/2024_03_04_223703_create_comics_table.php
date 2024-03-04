75468222832b9c73ff05fcc2618b15db3eed7a97
costanzaguala <costanzasilviaguala@gmail.com>
2024-03-05 00:04:06 +0100

    definizione tabella database

--- database/migrations/2024_03_04_223703_create_comics_table.php
+++ database/migrations/2024_03_04_223703_create_comics_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title',  256);
+            $table->string('description', 1024);
+            $table->string('thumb', 1024)->nullable();
+            $table->unsignedDecimal('price', 8, 2);
+            $table->string('series', 64);
+            $table->date('sale_date')->nullable();
+            $table->string('type',32);
+            $table->text('artists')->nullable();
+            $table->text('writers')->nullable(); 
             $table->timestamps();
         });
     }
